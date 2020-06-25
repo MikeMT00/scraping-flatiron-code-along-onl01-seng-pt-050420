b7c7b9990dffacc894914de980763aff8e22bade
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-25 00:05:05 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,28 +1,20 @@
-require 'nokogiri'
-require 'open-uri'
-
-require 'pry'
-
-require_relative './course.rb'
+class Course
+    attr_accessor :title, :description, :schedule
 
 
-class Scraper
+    @@all = []
 
-  def get_page
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end
-
-  def get_courses
-    self.get_page.css(".post")
-  end
-
-
-
-  def make_courses
-    self.get_courses.each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+    def initialize
+        @@all << self
     end
-  end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+
+
+end
